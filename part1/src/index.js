448139ed81ba8a50eaf919258c5017290eef8d5c
Simeon <adegbolasimeon@gmail.com>
2019-08-10 23:25:27 +0100

    exercise 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,17 +7,19 @@ const Header = (prop) => (
   </>
 );
 
-const Content = (prop) => (
+const Part = (prop) => (
   <>
     <p>
-      {prop.part1} {prop.exercises1}
+      {prop.part} {prop.exercises}
     </p>
-    <p>
-      {prop.part2} {prop.exercises2}
-    </p>
-    <p>
-      {prop.part3} {prop.exercises3}
-    </p>
+  </>
+);
+
+const Content = (prop) => (
+  <>
+    <Part part={prop.part1} exercises={prop.exercises1} />
+    <Part part={prop.part2} exercises={prop.exercises2} />
+    <Part part={prop.part3} exercises={prop.exercises3} />
   </>
 );
 
